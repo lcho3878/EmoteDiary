76cc5bca40c1005ba89172719e9bcb92d3947428
lcho3878 <leech3878@naver.com>
2024-05-19 15:58:25 +0900

    타겟 디바이스 및 버전 변경

--- EmoteDiary/ViewController.swift
+++ EmoteDiary/ViewController.swift
@@ -7,13 +7,68 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class MainViewController: UIViewController {
+//    private var counts = Array(repeating: 0, count: 9)
+    private var counts = [["행복해", 0],
+                          ["사랑해", 0],
+                          ["좋아해", 0],
+                          ["당황해", 0],
+                          ["속상해", 0],
+                          ["우울해", 0],
+                          ["심심해", 0],
+                          ["짜증나", 0],
+                          ["열받아", 0],
+    ]
+    private var buttonList: [UIButton] = []
+    private var labelList: [UILabel] = []
+    
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    @IBOutlet var button4: UIButton!
+    @IBOutlet var button5: UIButton!
+    @IBOutlet var button6: UIButton!
+    @IBOutlet var button7: UIButton!
+    @IBOutlet var button8: UIButton!
+    @IBOutlet var button9: UIButton!
+    
+    
+    @IBOutlet var label1: UILabel!
+    @IBOutlet var label2: UILabel!
+    @IBOutlet var label3: UILabel!
+    @IBOutlet var label4: UILabel!
+    @IBOutlet var label5: UILabel!
+    @IBOutlet var label6: UILabel!
+    @IBOutlet var label7: UILabel!
+    @IBOutlet var label8: UILabel!
+    @IBOutlet var label9: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        labelList = [label1, label2, label3, label4, label5, label6, label7, label8, label9]
+        buttonList = [button1, button2, button3, button4, button5, button6, button7, button8, button9]
+        
+        for (index, button) in buttonList.enumerated() {
+            settingButton(index, button)
+        }
 
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        let index = sender.tag
+        counts[index][1] = counts[index][1] as! Int + 1
+
+        labelList[index].text = "\(counts[index][0]) \(counts[index][1])"
+    }
+    
+    private func settingButton(_ i: Int, _ button: UIButton) {
+        button.tag = i
+        button.setImage(UIImage(named: "slime\(i+1)"), for: .normal)
+        let title = counts[i][0]
+        let count = counts[i][1]
+        labelList[i].text = "\(title) \(count)"
+        labelList[i].textAlignment = .center
+    }
 
 }
 
